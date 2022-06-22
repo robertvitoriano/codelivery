3dbbdc46793cdbf01e7eeed7a593431a4a1b2b7b
Robert Vitoriano <robertvitoriano@gmail.com>
2022-06-22 12:43:48 -0300

    add kafka consumer

---
+++ go-service/infra/kafka/consumer.go
@@ -0,0 +1,35 @@
+package kafka
+
+import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+
+type KafkaConsumer struct {
+	MsgChan chan  *ckafka.Message
+}
+
+func(k *KafkaConsume) Consume (){
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":os.Getenv("KafkaConsumerGroupId")
+	}
+
+	c, err ckafka.NewConsumer(configMap)
+
+	if err != nil {
+		log.fatal("Error consulting kafka message")
+	}
+	topics := []string{os.Getenv("KafkaReadTopic")}
+	c.SubscribeTopics(topics,nil)
+
+	fmt.Println("Kafka consumer has been started"
+
+	for{
+		msg, err := c.ReadMessage(-1)
+
+		if err == nil {
+			k.MsgChan <- msg
+		}
+	}
+)
+
+
+}
